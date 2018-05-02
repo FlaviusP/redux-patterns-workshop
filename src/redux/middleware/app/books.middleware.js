ed997ef3399da987d69e7192a4ee754dd29a287e
nirkaufman <nirkaufman@gmail.com>
2018-05-01 22:33:39 -0400

    refactoring

--- src/redux/middleware/app/books.middleware.js
+++ src/redux/middleware/app/books.middleware.js
@@ -1,18 +1,16 @@
 import {FETCH_BOOKS} from "../../actions/books.actions";
 import {apiRequest} from "../../actions/api.actions";
 import {setLoader} from "../../actions/ui.actions";
+import {BOOKS} from '../../actions/books.actions';
 
-const BOOKS = '[books]';
-
-export const booksMiddleware = ({getState, dispatch}) => (next) => (action) => {
+export const booksMiddleware = () => (next) => (action) => {
   next(action);
 
   switch (action.type) {
 
     case FETCH_BOOKS:
-      next([
-        apiRequest(null, 'GET', '/books', BOOKS),
-        setLoader(true, BOOKS)
-      ]);
+      next(apiRequest(null, 'GET', '/books', BOOKS));
+      next(setLoader(true, BOOKS));
+      break;
   }
 };
